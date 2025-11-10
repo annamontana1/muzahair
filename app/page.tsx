a97a73856e34f3563739f64e6c06b4508e419634
annamontana1 <zvin.a@seznam.cz>
2025-11-10 19:18:41 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: 100 }}>
+      <h1>Vítejte v Mùza Hair 💫</h1>
+      <p>Váš nový e-shop je online.</p>
+    </main>
+  );
+}
